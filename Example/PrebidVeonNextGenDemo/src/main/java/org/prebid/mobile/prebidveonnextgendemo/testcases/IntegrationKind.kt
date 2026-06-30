9ec14eb6dfef65d256a9263104f7abdbea89a2ad
jahonov <asilbekjahonov2001@gmail.com>
2026-06-30 15:02:42 +0500

    next gen deleted

--- Example/PrebidVeonNextGenDemo/src/main/java/org/prebid/mobile/prebidveonnextgendemo/testcases/IntegrationKind.kt
+++
@@ -1,9 +0,0 @@
-package org.prebid.mobile.prebidveonnextgendemo.testcases
-
-enum class IntegrationKind(
-    val adServer: String
-) {
-    ORIGINAL("Original API"),
-    RENDERING("Rendering API"),
-    MULTI("Multi SDK");
-}
